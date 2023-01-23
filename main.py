d6928b4e5436ccadf375d552bcd0f9d1e768c1f9
ArsenAD <khan071094@gmail.com>
2023-01-23 16:02:44 +0600

    hw

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from aiogram.utils import executor
-from config import dp
+from config import dp, bot, ADMINS
 import logging
 from handlers import client, callback, extra, admin, FsmAdminmentor
+
+
+async def on_startup(_):
+    await bot.send_message(chat_id=ADMINS[0],
+                           text='Bot started!')
+
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
@@ -12,4 +18,5 @@ extra.register_handlers_extra(dp)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True,
+                           on_startup=on_startup)
